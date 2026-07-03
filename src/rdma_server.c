8cef4650664b1d88cbec44a591b908205f25cf07
CarlYarwood <cdy8858@truman.edu>
2026-07-03 16:09:52 +0100

    f

--- src/rdma_server.c
+++ src/rdma_server.c
@@ -341,7 +341,7 @@ int main(int argc, char** argv) {
 
             case RDMA_CM_EVENT_ESTABLISHED :
                 printf("pre get ctx\n");
-                ctx = get_ctx_by_id(ctx_arr, (node_id *)(cm_event->id)->context);
+                ctx = get_ctx_by_id(ctx_arr, (struct node_id *)(cm_event->id)->context);
                 printf("post get ctx\n");
                 if(!ctx) {
                     perror("Failed to retreive context");
