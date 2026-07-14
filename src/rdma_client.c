24b59829ec72231417622c7f9355fcaa054baf3b
CarlYarwood <cdy8858@truman.edu>
2026-07-14 14:34:59 +0100

    fix debug text

--- src/rdma_client.c
+++ src/rdma_client.c
@@ -622,6 +622,7 @@ int acquire_lock(struct c_mcs_ctx ** ctx_arr,uint64_t *node_id, uint64_t *buffer
     while(metadata[NOTIFY] == 0) {
         // printf("node %lu waiting on Notify\n", *node_id);
     }
+    printf("node %lu after wait on notify", *node_id);
     return 0;
 }
 
