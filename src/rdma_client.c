10e42ea20609a4ead4ebcb945b187b1c3259f788
CarlYarwood <cdy8858@truman.edu>
2026-07-14 13:47:41 +0100

    modifying error text

--- src/rdma_client.c
+++ src/rdma_client.c
@@ -609,14 +609,14 @@ int acquire_lock(struct c_mcs_ctx ** ctx_arr,uint64_t *node_id, uint64_t *buffer
     if (*buffer == 0) {
         return 0;
     }
-    uint64_t expected = *buffer;
+    uint64_t expected;
     do {
         expected = *buffer;
         copmare_and_swap(ctx_arr[SERVER], expected, *node_id, LOCK);
     } while(expected != *buffer);
     copmare_and_swap(ctx_arr[*buffer], 0, *node_id, NEXT);
     while(metadata[NOTIFY] == 0) {
-        printf("node %lu waiting on Notify\n", *node_id);
+        // printf("node %lu waiting on Notify\n", *node_id);
     }
     return(0);
 }
