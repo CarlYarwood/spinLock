2a83a010d18f69c9389f82793e51770e59f51ca0
CarlYarwood <cdy8858@truman.edu>
2026-07-15 11:58:25 +0100

    fix print statement

--- src/rdma_client.c
+++ src/rdma_client.c
@@ -620,7 +620,7 @@ int acquire_lock(struct c_mcs_ctx ** ctx_arr,uint64_t *node_id, uint64_t *buffer
     }
     printf("node %lu lock contested registering next\n", *node_id);
     copmare_and_swap(ctx_arr[*buffer], 0, *node_id, NEXT);
-    printf("node %lu cas response\n", *node_id);
+    printf("node %lu cas response %lu\n", *node_id, *buffer);
     printf("node %lu wait on notify\n", *node_id);
     while(metadata[NOTIFY] == 0) {
         // printf("node %lu waiting on Notify\n", *node_id);
